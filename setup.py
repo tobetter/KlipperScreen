3cd708552cfef838d0c3fa35dc6a9b4fd45139ed
Dongjin Kim <tobetter@gmail.com>
2021-02-07 09:52:50 +0000

    (DO NOT MERGE) Debianize KlipperScreen
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'klipperscreen',
+        version = '0.1.6',
+        description = 'Klipper Screen',
+        author = 'Jordan Ruthe',
+        author_email = 'jordan.ruthe@gmail.com',
+        license = 'GPLv3',
+        packages = find_packages(),
+        install_requires = [
+            'requests==2.25.1',
+            'vext==0.7.4',
+            'websocket-client==0.57.0',
+            'humanize==3.2.0',
+            'jinja2==2.11.2'
+            ],
+        include_package_data = True,
+        zip_safe = False)
